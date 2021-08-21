5a44fb0da131480ca9789e10646cb7b21f96fc42
Cj-bc <cj.bc-sd@outlook.jp>
2021-08-22 00:05:35 +0900

    Add: Data.VRM> subset of VRM data types for this module
    It should not used as 'common data type' for VRM programs

---
+++ src/Data/VRM.hs
@@ -0,0 +1,26 @@
+{- |
+Module      :  Data.VRM
+Description :  Data types for subset of VRM
+Copyright   :  (c) Cj.bc-sd a.k.a. Cj-bc
+License     :  GPL-3
+Maintainer  :  cj.bc-sd@outlook.jp
+Stability   :  experimental
+Portability :  portable
+
+Subset of data types for VRM
+-}
+
+module Data.VMCP.Marionette where
+import Data.Text (Text)
+
+-- | Predefined Blendshape names.
+--
+-- ref: https://github.com/vrm-c/vrm-specification/blob/master/specification/0.0/README.ja.md#ブレンドシェイプグループjsonextensionsvrmblendshapemasterblendshapegroups 
+data BlendShapeExpression = Neutral
+                          | A | I | U | E | O
+                          | Blink
+                          | Joy | Angry | Sorrow | Fun
+                          | LookUp | LookDown
+                          | LookLeft | LookRight
+                          | BlinkL | BlinkR
+                          | Custom Text

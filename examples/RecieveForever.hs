1f9c2b571b73f32ad78331cf1a821d0f7c1f3cd1
Cj-bc <cj.bc-sd@outlook.jp>
2022-07-25 15:41:12 +0900

    Add: examples/RecieveForever> New example executable

---
+++ examples/RecieveForever.hs
@@ -0,0 +1,15 @@
+{-
+Assume VMC Performer uses 192.168.10.3:39540 to send data,
+and read them forever
+
+VMC Performerが 192.168.10.3:39540 にデータを送信している前提で,
+送られてきたデータを永続的に表示します
+-}
+module Main where
+
+import Pipes
+import qualified Pipes.Prelude as P
+import Pipes.VMCP.Marionette
+
+main = runEffect $ recvMarionetteMsg "192.168.10.3" 39540
+       >-> P.print
